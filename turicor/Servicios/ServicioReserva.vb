c5fd69c5b3929bf3ab736c468423b3827c14dd88
Ange Tresca <angetresca@bitbucket.org>
2019-06-04 17:32:51 -0300

    Agrego servicios

---
+++ turicor/Servicios/ServicioReserva.vb
@@ -0,0 +1,14 @@
+﻿Public Class ServicioReserva
+
+    Private cliente As ServicioWCF.WCFReservaVehiculosClient
+    Public Property propCliente() As ServicioWCF.WCFReservaVehiculosClient
+        Get
+            Return cliente
+        End Get
+        Set(ByVal value As ServicioWCF.WCFReservaVehiculosClient)
+            cliente = value
+        End Set
+    End Property
+
+
+End Class
---
+++ turicor/Servicios/ServicioVehiculo.vb
@@ -0,0 +1,13 @@
+﻿Public Class ServicioVehiculo
+
+    Private cliente As ServicioWCF.WCFReservaVehiculosClient
+    Public Property propCliente() As ServicioWCF.WCFReservaVehiculosClient
+        Get
+            Return cliente
+        End Get
+        Set(ByVal value As ServicioWCF.WCFReservaVehiculosClient)
+            cliente = value
+        End Set
+    End Property
+
+End Class
