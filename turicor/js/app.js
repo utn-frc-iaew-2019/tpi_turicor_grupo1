bf0f7e34cdd52836a6a1af66d39d7be33456b39b
Irina Rossi <irinarossi11@gmail.com>
2019-06-18 18:05:19 -0300

    Incluir modelo BD

---
+++ turicor/js/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module("app", [])
+
+app.controller("ClientesController", function ($scope, $http) {
+    $scope.articulos = [];
+
+    $http.get('http://localhost:57801/api/Empleados').then(
+        function (response) {
+            debugger;
+            $scope.clientes = response.data;
+        })
+
+})
