0d5416c5fd0bd165203290eddcc8306234e19ff9
Ange Tresca <angetresca@bitbucket.org>
2019-06-18 21:00:44 -0300

    reordeno archivos js y agrego js vechiculos disponibles

--- turicor/javascript/home.js
+++ turicor/javascript/home.js
@@ -19,18 +19,3 @@ function signOut() {
         console.log('Sesión cerrada.');
     });
 }
-
-$(document).ready(function () {
-
-    $.ajax({
-        url: 'http://localhost:55669/api/Vehiculo',
-        success: function (respuesta) {
-            console.log("exitooo");
-            console.log(respuesta);
-        },
-        error: function () {
-            console.log("No se ha podido obtener la información");
-        }
-    });
-
-});
