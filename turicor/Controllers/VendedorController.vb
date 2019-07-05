0d4d1c81fc8ab9fc0cde17069888f33a977975dc
Irina Rossi <irinarossi11@gmail.com>
2019-07-05 20:49:17 -0300

    Consulta BD

---
+++ turicor/Controllers/VendedorController.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Net
+Imports System.Web.Http
+
+Namespace Controllers
+    Public Class VendedorController
+        Inherits ApiController
+
+        ' GET: api/Vendedor
+        Public Function GetValues()
+            Dim servicioVendedor As New ServicioVendedor
+            Return servicioVendedor.consultarVendedores()
+        End Function
+
+        ' GET: api/Vendedor/5
+        Public Function GetValue(ByVal id As Integer) As String
+            Return "value"
+        End Function
+
+        ' POST: api/Vendedor
+        Public Sub PostValue(<FromBody()> ByVal value As String)
+
+        End Sub
+
+        ' PUT: api/Vendedor/5
+        Public Sub PutValue(ByVal id As Integer, <FromBody()> ByVal value As String)
+
+        End Sub
+
+        ' DELETE: api/Vendedor/5
+        Public Sub DeleteValue(ByVal id As Integer)
+
+        End Sub
+    End Class
+End Namespace
---
+++ turicor/Servicios/ServicioVendedor.vb
@@ -0,0 +1,42 @@
+﻿Public Class ServicioVendedor
+    Private credenciales As New ServicioWCF.Credentials
+    Private cliente As New ServicioWCF.WCFReservaVehiculosClient
+    Dim cadena_conexion As String = "Provider=SQLNCLI11;Data Source=IRINA\BDSERVER12;Integrated Security=SSPI;Initial Catalog=TuricorBD"
+
+
+    Public Sub New()
+        credenciales.UserName = "grupo_nro1"
+        credenciales.Password = "AzArobmjV0"
+    End Sub
+
+    Public Function consultarVendedores()
+        Dim sql As String = " "
+        Dim conexion As New OleDb.OleDbConnection
+        Dim cmd As New OleDb.OleDbCommand
+        Dim tabla As New Data.DataTable
+        Dim reserva As New Reserva
+
+        conexion.ConnectionString = cadena_conexion
+        conexion.Open()
+        cmd.Connection = conexion
+        cmd.CommandType = CommandType.Text
+
+
+        sql = " SELECT * FROM Vendedor"
+
+        cmd.CommandText = sql
+        tabla.Load(cmd.ExecuteReader())
+        conexion.Close()
+
+        Dim vendedores As New ArrayList
+        Dim c As Integer = 0
+        For c = 0 To tabla.Rows.Count - 1
+            Dim vendedor As New Vendedor
+            vendedor.idP = tabla.Rows(c)(0)
+            vendedor.nombreP = tabla.Rows(c)(1)
+            vendedores.Add(vendedor)
+        Next
+
+        Return vendedores
+    End Function
+End Class
